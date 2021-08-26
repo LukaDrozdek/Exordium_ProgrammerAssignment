c5d1419bda9e2777c1b0290956cd73af572c16d6
LukaDrozdek <Luka04111993@gmail.com>
2021-08-26 02:41:34 +0200

    1. 2D player moves
    Basic Movment, animation adn collider

---
+++ Assets/Script/Player/PlayerMovment.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    [Header("Player Movment")]
+    public float speed = 5;
+    private Rigidbody2D rb;
+
+    [Header("Player Animator")]
+    public Animator animator;
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+
+    void MovePlayer()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float yMove = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(xMove, yMove).normalized * speed * Time.deltaTime;
+
+        SetAnimation(xMove, yMove);
+    }
+
+    void SetAnimation(float xMove, float yMove)
+    {
+        animator.SetFloat("PlayerSpeedX", xMove);
+        animator.SetFloat("PlayerSpeedY", yMove);
+    }
+}
