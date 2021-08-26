9e5326a13cce091821b6b97509fb8ac76d64b189
LukaDrozdek <Luka04111993@gmail.com>
2021-08-26 21:45:37 +0200

    Inventory
    Pick up object and stack.

---
+++ Assets/Script/Scriptable Objects/Inventory/Scripts/DisplayInventory.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayInventory : MonoBehaviour
+{
+
+    public InventoryObject inventory;
+    public int xGridStart;
+    public int yGridStart;
+    public int xSpaceBetweenItem;
+    public int ySpaceBetweenItem;
+    public int numberOfColumn;
+    Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
+
+    void Start()
+    {
+        CreateDisplay();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateDisplay();
+    }
+
+    public void UpdateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            if(itemsDisplayed.ContainsKey(inventory.Container[i]))
+            {
+                itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+
+            }
+            else
+            {
+                var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+                itemsDisplayed.Add(inventory.Container[i], obj);
+            }
+
+        }
+    }
+
+    public void CreateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+            obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+            obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            itemsDisplayed.Add(inventory.Container[i], obj);
+        }
+    }
+
+    public Vector3 GetPosition(int i)
+    {
+        //print(xGridStart + (xSpaceBetweenItem * (i % numberOfColumn)) + "X");
+        //print(yGridStart + (-ySpaceBetweenItem * (i / numberOfColumn)));
+        return new Vector3(xGridStart + (xSpaceBetweenItem * (i % numberOfColumn)),yGridStart + (-ySpaceBetweenItem * (i/numberOfColumn)), 0f);
+    }
+}
